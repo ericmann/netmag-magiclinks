b7b5867754342d33bf5b3ef83abbefca893edaff
Eric Mann <eric@eamann.com>
2016-10-22 19:52:30 -0700

    Flesh out routing

--- slim/src/middleware.php
+++ slim/src/middleware.php
@@ -1,4 +1,108 @@
 <?php
-// Application middleware
+// Route-specific Middleware
+class PasswordAuthentication {
+	/**
+	 * @var \Slim\Container
+	 */
+	private $container;
 
-// e.g: $app->add(new \Slim\Csrf\Guard);
+	public function __construct( $container ) {
+		$this->container = $container;
+	}
+
+	/**
+	 * If a username/password pair are submitted, authenticate that way.
+	 *
+	 * @param \Slim\Http\Request  $request
+	 * @param \Slim\Http\Response $response
+	 * @param callable            $next
+	 *
+	 * @return \Slim\Http\Response
+	 */
+	public function __invoke($request, $response, $next) {
+		$username = $request->getParam( 'username' );
+		$password = $request->getParam( 'password' );
+
+		// If no username/password, more to the next stack
+		if ( empty($username) || empty($password) ) {
+			$response = $next( $request, $response );
+		} else {
+			$user = $this->container->users->get($username);
+
+			if ( $user ) {
+				/**
+				 * Once we have a user, we need to compare the provided password (during login) with the stored
+				 * hash in the database. If they match, great! If not, move on and set a generic "invalid login"
+				 * error.
+				 */
+				$user_data = json_decode( $user, true );
+				if ($this->container->passwordhasher->checkPassword( $password, $user_data['password'] ) ) {
+					$_SESSION['username'] = $username;
+
+					return $response = $response->withRedirect('/authenticated');
+				}
+			}
+			return $response = $response->withRedirect('/?error=invalidlogin');
+		}
+
+		return $response;
+	}
+}
+
+class MagicLinkAuthentication {
+	/**
+	 * @var \Slim\Container
+	 */
+	private $container;
+
+	public function __construct( $container ) {
+		$this->container = $container;
+	}
+
+	/**
+	 * If a toznyo/toznyr pair are submitted, authenticate that way.
+	 *
+	 * @param \Slim\Http\Request  $request
+	 * @param \Slim\Http\Response $response
+	 * @param callable            $next
+	 *
+	 * @return \Slim\Http\Response
+	 */
+	public function __invoke($request, $response, $next) {
+		$toznyo = $request->getQueryParam('toznyo');
+		$toznyr = $request->getQueryParam('toznyr');
+
+		// If no toznyo/toznyr, more to the next stack
+		if ( empty($toznyo) || empty($toznyr) ) {
+			$response = $next( $request, $response );
+		} else {
+			$validated = $this->container->tozny_user->userLinkResult($toznyo);
+
+			if ( isset( $validated['signed_data'] ) ) {
+				$data = $validated['signed_data'];
+
+				$decoded = Tozny_Remote_Realm_API::base64UrlDecode( $data );
+				$deserialized = json_decode( $decoded, true );
+
+				if ( isset( $deserialized['data'] ) ) {
+					$realm_data = json_decode( $deserialized['data'], true );
+
+					if ( isset( $realm_data['username'] ) ) {
+						$username = $realm_data['username'];
+						$user = $this->container->users->get( $username );
+
+						if ( $user ) {
+							$_SESSION['username'] = $username;
+
+							return $response = $response->withRedirect('/authenticated');
+						}
+					}
+				}
+			}
+
+			$response = $next( $request, $response );
+		}
+
+		return $response;
+	}
+}
--- slim/src/routes.php
+++ slim/src/routes.php
@@ -1,10 +1,108 @@
 <?php
+// Get a DI reference
+$container = $app->getContainer();
+
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function($request, $response, $args) {
+	/** @var \Slim\Http\Request $request */
+	/** @var \Slim\Http\Response $response */
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+	$error = $request->getQueryParam( 'error' );
+	if ( ! empty( $this->errors[ $error ] ) ) {
+		$args[ 'error' ] = $this->errors[ $error ];
+	}
+	$message = $request->getQueryParam( 'message' );
+	if ( ! empty( $this->messages[ $message ] ) ) {
+		$args[ 'message' ] = $this->messages[ $message ];
+	}
+	// Render index view
+	return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/register', function($request, $response, $args) {
+	/** @var \Slim\Http\Request $request */
+	/** @var \Slim\Http\Response $response */
+
+	$error = $request->getQueryParam( 'error' );
+	if ( ! empty( $this->errors[ $error ] ) ) {
+		$args[ 'error' ] = $this->errors[ $error ];
+	}
+	// Render registration view
+	return $this->renderer->render($response, 'register.phtml', $args);
+});
+$app->post('/register', function($request, $response, $args) {
+	/** @var \Slim\Http\Request $request */
+	/** @var \Slim\Http\Response $response */
+
+	$fname = $request->getParam('fname');
+	$lname = $request->getParam('lname');
+	$username = $request->getParam('username');
+	$email = $request->getParam('email');
+	$password = $request->getParam('password');
+	$cpassword = $request->getParam('password_confirm');
+	if ( $this->users->get( $username ) ) {
+		return $response->withRedirect('/register?error=useduser');
+	}
+	if ( empty( $email ) ) {
+		return $response->withRedirect('/register?error=emptyemail');
+	}
+	if (! hash_equals($password, $cpassword) ) {
+		return $response->withRedirect('/register?error=nomatch');
+	}
+	$user = [
+		'firstName' => $fname,
+		'lastName'  => $lname,
+		'username'  => $username,
+		'email'     => $email,
+		'password'  => $this->passwordhasher->HashPassword( $password ),
+	];
+	$this->users->set( $username, json_encode( $user ) );
+
+	$_SESSION['username'] = $username;
+
+	return $response->withRedirect('/authenticated');
+});
+
+$app->any('/login', function($request, $response, $args) {
+	/** @var \Slim\Http\Request $request */
+	/** @var \Slim\Http\Response $response */
+var_dump($request);
+	return $response->withRedirect('/?error=invalidlogin');
+})->add( new PasswordAuthentication($container) )->add( new MagicLinkAuthentication($container) );
+
+$app->get('/authenticated', function($request, $response, $args) {
+	/** @var \Slim\Http\Request $request */
+	/** @var \Slim\Http\Response $response */
+
+	if ( ! isset( $_SESSION['username'] ) || ! $this->users->get( $_SESSION['username'] ) ) {
+		return $response->withRedirect('/?error=notloggedin');
+	}
+	$error = $request->getQueryParam( 'error' );
+	if ( ! empty( $this->errors[ $error ] ) ) {
+		$args[ 'error' ] = $this->errors[ $error ];
+	}
+	$message = $request->getQueryParam( 'message' );
+	if ( ! empty( $this->messages[ $message ] ) ) {
+		$args[ 'message' ] = $this->messages[ $message ];
+	}
+
+	$user_data = $this->users->get( $_SESSION['username'] );
+	$user = json_decode( $user_data, true );
+	$args['fName'] = $user['firstName'];
+	$args['lName'] = $user['lastName'];
+
+	return $this->renderer->render($response, 'authenticated.phtml', $args);
+});
+
+/**
+ * Logging out is a matter of clearing the PHP session and redirecting to the homepage.
+ */
+$app->get('/logout', function($request, $response, $args) {
+	/** @var \Slim\Http\Request $request */
+	/** @var \Slim\Http\Response $response */
+
+	session_destroy();
+
+	return $response->withRedirect('/?loggedout');
+} );
